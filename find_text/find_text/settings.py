ac421c18a4ed65dbee70dc3e8a757263146b48c5
Muthusaravanan V <166006314+Muthusaravanan85@users.noreply.github.com>
2025-04-21 22:23:26 +0530

    Update settings.py
    updating debug

--- find_text/find_text/settings.py
+++ find_text/find_text/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4hgcmkvzgb=zvcw8(*rlaw^ba=7mf4(w3egtt)f_)=ahkj#b15'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my_text.txt.com','127.0.0.1']
 
 
 # Application definition
